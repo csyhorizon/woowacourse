45d1ea36910b5766d36f7ff7cb2501b3a3798b09
chino <tnehd83@naver.com>
2024-11-11 13:09:26 +0900

    feat(common): Error, Input, Output Message Enum 추가

--- src/main/java/common/ErrorMessage.java
+++ src/main/java/common/ErrorMessage.java
@@ -1,4 +1,19 @@
 package common;
 
 public enum ErrorMessage {
+    NONE_INPUT("입력을 받지 못했습니다"),
+    NONE_PRODUCT("존재하지 않는 상품입니다"),
+    SOLD_OUT_PRODUCT("매진된 상품입니다"),
+    INVALID_INPUT("잘못된 형태의 입력입니다");
+
+
+    private final String message;
+
+    ErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
--- src/main/java/common/OutputMessage.java
+++ src/main/java/common/OutputMessage.java
@@ -1,4 +1,24 @@
 package common;
 
 public enum OutputMessage {
+    INTRO_MESSAGE("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다."),
+    RECEIPT_NAME("==============W 편의점================"),
+    RECEIPT_PROMOTIONS("=============증\t정==============="),
+    RECEIPT_SLASH("===================================="),
+    RECEIPT_BUY_NAME("상품명\t\t수량\t금액"),
+    RECEIPT_TOTAL("총구매액"),
+    RECEIPT_SALES("행사할인"),
+    RECEIPT_MEMBERSHIP("멤버십할인"),
+    RECEIPT_PAY_MONEY("내실돈");
+
+
+    private final String message;
+
+    OutputMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
 }
