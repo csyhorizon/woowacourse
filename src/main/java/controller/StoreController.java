4a0e940cb19b7acec6b935be1c95ced436f2569c
chino <tnehd83@naver.com>
2024-11-11 12:47:35 +0900

    feat: 기본 파일 추가

---
+++ src/main/java/controller/StoreController.java
@@ -0,0 +1,15 @@
+package controller;
+
+import services.StoreService;
+
+public class StoreController {
+    private StoreService storeService;
+
+    public StoreController(StoreService storeService) {
+        this.storeService = storeService;
+    }
+
+    public void run() {
+
+    }
+}
--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,12 @@
 package store;
 
+import controller.StoreController;
+import services.StoreService;
+
 public class Application {
     public static void main(String[] args) {
-
+        StoreService storeService = new StoreService();
+        StoreController storeController = new StoreController(storeService);
+        storeController.run();
     }
 }
