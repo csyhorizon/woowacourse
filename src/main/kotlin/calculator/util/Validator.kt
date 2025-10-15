348536c14263e90bd3897b126c2f133f89e64507
csy <tnehd83@naver.com>
2025-10-15 13:41:47 +0900

    feat(validator): 숫자, 구분자, 커스텀 구분자 이외의 값 입력 시 예외 발생

--- src/main/kotlin/calculator/util/Validator.kt
+++ src/main/kotlin/calculator/util/Validator.kt
@@ -8,4 +8,19 @@ object Validator {
             throw IllegalArgumentException("음수 입력 불가: $negatives")
         }
     }
+
+    fun checkInvalidCharacters(input: String, customDelimiter: String?) {
+        var allowedChars = "0-9,:\n"
+
+        if (customDelimiter != null) {
+            val escapedDelimiter = Regex.escape(customDelimiter)
+            allowedChars += escapedDelimiter;
+        }
+
+        val regex = Regex("""[^$allowedChars]""")
+
+        if (regex.containsMatchIn(input)) {
+            throw IllegalArgumentException("잘못된 문자 포함: $input")
+        }
+    }
 }
