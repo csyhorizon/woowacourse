0b28a73b784a715e55112ceaea5d536de267647c
csy <tnehd83@naver.com>
2025-11-08 21:02:08 +0900

    feat:default setting

---
+++ src/main/java/orinnetwork/javafilter8/JavaFilter8Application.java
@@ -0,0 +1,13 @@
+package orinnetwork.javafilter8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaFilter8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaFilter8Application.class, args);
+    }
+
+}
