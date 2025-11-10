e44dc62bb5f31df45b837f141e2693fa1a3c9b66
csy <tnehd83@naver.com>
2025-11-10 19:57:10 +0900

    feat(post): Post 엔티티 및 PostStatus enum 추가

--- src/main/java/orinnetwork/javafilter8/domain/Post.java
+++ src/main/java/orinnetwork/javafilter8/domain/Post.java
@@ -1,4 +1,46 @@
 package orinnetwork.javafilter8.domain;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String title;
+
+    @Column(nullable = false, columnDefinition = "TEXT")
+    private String content;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PostStatus postStatus;
+
+    @Builder
+    public Post(String title, String content, PostStatus postStatus) {
+        this.title = title;
+        this.content = content;
+        this.postStatus = postStatus;
+    }
+
+    public void update(String title, String content, PostStatus postStatus) {
+        this.title = title;
+        this.content = content;
+        this.postStatus = postStatus;
+    }
 }
--- src/main/java/orinnetwork/javafilter8/domain/PostStatus.java
+++ src/main/java/orinnetwork/javafilter8/domain/PostStatus.java
@@ -1,4 +1,8 @@
 package orinnetwork.javafilter8.domain;
 
 public enum PostStatus {
+    ACTIVE,     // 공개 상태
+    PENDING,    // 대기 상태
+    REJECTED,   // 금지 상태
+    DELETED,    // 삭제
 }
