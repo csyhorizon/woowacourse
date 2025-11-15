f8f1a75fa27fe467fb130ca2c8b3eba1f65c6ec3
csy <tnehd83@naver.com>
2025-11-16 00:00:03 +0900

    feat(filterconsumer): 필터링 요청 메시지 소비자 구현

--- src/main/java/orinnetwork/javafilter8/consumer/FilterConsumer.java
+++ src/main/java/orinnetwork/javafilter8/consumer/FilterConsumer.java
@@ -1,4 +1,38 @@
 package orinnetwork.javafilter8.consumer;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+import orinnetwork.javafilter8.dto.PostEvent;
+import orinnetwork.javafilter8.service.FilterService;
+
+
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class FilterConsumer {
+
+    private final FilterService filterService;
+
+    @RabbitListener(queues = "filtering.queue")
+    public void handleFilteringMessage(PostEvent eventDto) {
+
+        if (eventDto == null || eventDto.postId() == null) {
+            log.warn("수신된 메시지가 비어있습니다");
+            return;
+        }
+
+        Long postId = eventDto.postId();
+        log.info("필터링 요청 수신 : {}", postId);
+
+        try {
+            filterService.filterPost(postId);
+        } catch (IllegalArgumentException e) {
+            log.error("처리 불가능한 오류");
+        } catch (Exception e) {
+            throw new RuntimeException("메시지 처리 실패");
+        }
+    }
 }
