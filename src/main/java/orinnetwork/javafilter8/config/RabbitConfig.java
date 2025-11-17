45916ca70f686d28a0045c8db73fe990dd1688ed
csy <tnehd83@naver.com>
2025-11-17 12:54:43 +0900

    feat(rabbitconfig): RabbitConfig 설정 추가

---
+++ src/main/java/orinnetwork/javafilter8/config/RabbitConfig.java
@@ -0,0 +1,50 @@
+package orinnetwork.javafilter8.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitConfig {
+
+    public static final String POST_EXCHANGE_NAME = "post.exchange";
+    public static final String FILTER_QUEUE_NAME = "filter.queue";
+    public static final String POST_CREATED_ROUTING_KEY = "post.created";
+
+    @Bean
+    public TopicExchange postExchange() {
+        return new TopicExchange(POST_EXCHANGE_NAME);
+    }
+
+    @Bean
+    public Queue filterQueue() {
+        return new Queue(FILTER_QUEUE_NAME);
+    }
+
+    @Bean
+    public Binding binding(Queue filterQueue, TopicExchange postExchange) {
+        return BindingBuilder.bind(filterQueue)
+                .to(postExchange)
+                .with(POST_CREATED_ROUTING_KEY);
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory,
+                                         MessageConverter messageConverter) {
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(messageConverter);
+        return rabbitTemplate;
+    }
+
+    @Bean
+    public MessageConverter messageConverter() {
+        return new Jackson2JsonMessageConverter();
+    }
+}
