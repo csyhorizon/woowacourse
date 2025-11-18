3b8d778285c92b712fc46f249e38e4435407a478
csy <tnehd83@naver.com>
2025-11-18 23:23:03 +0900

    feat(rabbitconfig): DLQ 설정 추가

--- src/main/java/orinnetwork/javafilter8/config/RabbitConfig.java
+++ src/main/java/orinnetwork/javafilter8/config/RabbitConfig.java
@@ -3,6 +3,7 @@ package orinnetwork.javafilter8.config;
 import org.springframework.amqp.core.Binding;
 import org.springframework.amqp.core.BindingBuilder;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.QueueBuilder;
 import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
@@ -18,14 +19,31 @@ public class RabbitConfig {
     public static final String FILTER_QUEUE_NAME = "filter.queue";
     public static final String POST_CREATED_ROUTING_KEY = "post.created";
 
+    public static final String DEAD_EXCHANGE_NAME = "dead.exchange";
+    public static final String DEAD_QUEUE_NAME = "dead.queue";
+    public static final String DEAD_ROUTING_KEY = "dead.key";
+
     @Bean
     public TopicExchange postExchange() {
         return new TopicExchange(POST_EXCHANGE_NAME);
     }
 
     @Bean
+    public TopicExchange deadExchange() {
+        return new TopicExchange(DEAD_EXCHANGE_NAME);
+    }
+
+    @Bean
     public Queue filterQueue() {
-        return new Queue(FILTER_QUEUE_NAME);
+        return QueueBuilder.durable(FILTER_QUEUE_NAME)
+                .withArgument("x-dead-letter-exchange", DEAD_EXCHANGE_NAME)
+                .withArgument("x-dead-letter-routing-key", DEAD_ROUTING_KEY)
+                .build();
+    }
+
+    @Bean
+    public Queue deadQueue() {
+        return new Queue(DEAD_QUEUE_NAME);
     }
 
     @Bean
@@ -33,6 +51,13 @@ public class RabbitConfig {
         return BindingBuilder.bind(filterQueue)
                 .to(postExchange)
                 .with(POST_CREATED_ROUTING_KEY);
+    }
+
+    @Bean
+    public Binding deadBinding(Queue deadQueue, TopicExchange deadExchange) {
+        return BindingBuilder.bind(deadQueue)
+                .to(deadExchange)
+                .with(DEAD_ROUTING_KEY);
     }
 
     @Bean
